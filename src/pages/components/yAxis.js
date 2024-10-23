814fc95697b1cf038ba4bc11a194dd5ffc35a8df
Luviayao <yy4107@nyu.edu>
2024-10-23 13:43:56 +0800

    try11

--- src/pages/components/yAxis.js
+++ src/pages/components/yAxis.js
@@ -1,28 +1,33 @@
-function YAxis(props){
+function YAxis(props) {
     const { yScale, height, axisLable } = props;
-    const ticks = yScale.ticks();
-    //console.log("yaxisticks:",ticks)
-    if(yScale){
-        return <g>
-        <line y2={height} stroke={"black"}/>
-        {yScale.ticks().map(ticks => (
-            <g key={ticks} transform={`translate(-5, ${yScale(ticks)})`}>
-                <line x2={5} stroke={'black'}/>
-                <text style={{textAnchor:'end', fontSize:'10px'}}
-                x = {0}>
-                {ticks}
-                </text>
-            </g>
-        ))}
 
-            <text style={{ textAnchor:'end', fontSize:'15px'}} transform={`translate(20,0)rotate(-90)`}>
+    // Ensure yScale is defined and has ticks method
+    if (!yScale || !yScale.ticks) {
+        return <g></g>;
+    }
+
+    const ticks = yScale.ticks();
+    console.log("yaxisticks:", ticks);
+
+    return (
+        <g>
+            <line y2={height} stroke={"black"} />
+            {ticks.map(tick => (
+                <g key={tick} transform={`translate(-5, ${yScale(tick)})`}>
+                    <line x2={5} stroke={'black'} />
+                    <text style={{ textAnchor: 'end', fontSize: '10px' }} x={0}>
+                        {tick}
+                    </text>
+                </g>
+            ))}
+
+            <text
+                style={{ textAnchor: 'end', fontSize: '15px' }}
+                transform={`translate(20, 0)rotate(-90)`}
+            >
                 {axisLable}
             </text>
         </g>
-    } else {
-        return <g></g>
-    }
-
+    );
 }
-
 export default YAxis
